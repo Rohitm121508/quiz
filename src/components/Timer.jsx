04644b5bf352ddae103a5a7f705ed0ec72d8f14a
Rohitm121508 <2021uee1508@mnit.ac.in>
2023-12-12 14:33:09 +0530

    quiz app

---
+++ src/components/Timer.jsx
@@ -0,0 +1,24 @@
+import React, { useState,useEffect } from 'react'
+
+function Timer(setStop,QuestionNumber) {
+    const [timer,setTimer] =useState(30)
+
+    useEffect(() => {
+        if(timer==0){
+            return setStop(true)
+        }
+     const interval = setInterval(()=>{
+       setTimer((prev) => prev-1)
+     },1000)
+     return ()=> clearInterval(interval)
+    }, [setStop,timer])
+
+    useEffect(() => {
+    setTimer(30)
+    }, [QuestionNumber])
+  return (
+   timer
+  )
+}
+
+export default Timer
